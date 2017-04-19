95789c13ef4f8855c0cc0c7be8cefa6d157d78d3
IC071166 <IC071166@AAEINBR146703N.ad001.siemens.net>
2017-04-20 01:29:37 +0530

    Added hbase-site.xml, SAX Parser, Metrics Measurements Classes.

---
+++ BigCoding/src/uncat/MetricsMeasurements.java
@@ -0,0 +1,46 @@
+package uncat;
+
+import com.codahale.metrics.*;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.jasper.util.Queue;
+
+public class MetricsMeasurements {
+	static final MetricRegistry metrics = new MetricRegistry();
+	static Queue queue = new Queue();
+
+	public static void main(String args[]) {
+		startReport(); // configures for console printing at interval
+
+		Meter meter = metrics.meter("meter"); // what name it comes for
+		meter.mark();
+		meter.mark(3); // Number shows on measurement
+		Counter counter = metrics.counter("Counter");
+		counter.inc();
+		Histogram histogram = metrics.histogram("Histogram");
+		histogram.update(5);
+		Timer timer = metrics.timer("Timer");
+		timer.update(2L, TimeUnit.MILLISECONDS);
+		timer.time();
+		queue.put(1);
+		metrics.register(MetricRegistry.name(MetricsMeasurements.class, "Gauge"), new Gauge<Integer>() {
+			public Integer getValue() {
+				return queue.size();
+			}
+		});
+		wait5Seconds();
+	}
+
+	static void startReport() {
+		ConsoleReporter reporter = ConsoleReporter.forRegistry(metrics).convertRatesTo(TimeUnit.SECONDS)
+				.convertDurationsTo(TimeUnit.MILLISECONDS).build();
+		reporter.start(1, TimeUnit.SECONDS);
+	}
+
+	static void wait5Seconds() {
+		try {
+			Thread.sleep(5 * 1000);
+		} catch (InterruptedException e) {
+		}
+	}
+}
